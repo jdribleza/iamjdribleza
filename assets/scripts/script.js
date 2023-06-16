e54919f6f56fd789ea783ea9587e39f1fa275041
Jann Dervin Ribleza <136655670+jdribleza@users.noreply.github.com>
2023-06-16 12:06:15 +0800

    Update script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,52 +1,46 @@
-
-
-
-var close_btn = document.querySelector('.c-close-menu-btn > a');
+var main_header = document.querySelector('header');
+var menu_btn_cntnr = document.querySelector('.c-menu-btn-cntnr');
 var menu_btn = document.querySelector('.c-menu-btn');
-var menu_cntnr = document.querySelector('.c-open-menu');
-var menu_opxns = document.querySelector('.c-menu-list > li > a');
-var menu_contnr = document.querySelector('.c-menu-list');
-var navigation_panel = document.querySelector('header');
+var close_btn = document.querySelector('.c-close-menu-btn > a');
+var nav_link_cntnr = document.querySelector('.c-menu-list');
+var nav_link_btn = document.querySelector('.c-menu-list > li > a');
 var to_top = document.querySelector('.to-top');
 var fb_link = document.querySelector('.fb-link');
 
 //OPENS MENU WHEN MENU BUTTON CLICKED
 menu_btn.addEventListener('click', function(e){
     e.preventDefault();
-    menu_contnr.style.display = 'flex';
+    nav_link_cntnr.style.display = 'flex';
     this.style.display = 'none';
-    menu_contnr.style.position = 'fixed';
+    nav_link_cntnr.style.position = 'fixed';
 });
 
 //CLOSE MENU WHEN X BUTTON CLICKED
 close_btn.addEventListener('click', function(e){
     e.preventDefault();
-    menu_contnr.style.display = 'none';
+    nav_link_cntnr.style.display = 'none';
     menu_btn.style.display = 'flex';
 });
 
 //CLOSE MENU WHEN LINK CLICKED AND NAVIGATE
-menu_opxns.addEventListener('click', function(e){
-    e.preventDefault();
-    menu_contnr.style.display = 'none';
+function navigate(){
+    nav_link_cntnr.style.display = 'none';
     menu_btn.style.display = 'flex';
-});
+}
 
-//CHANGE NAVIGATION BAR BACKGROUND COLOR
-/*
-window.onscroll = function(){
-    if (window.pageYOffset > window.innerHeight-400){
-        navigation_panel.style.boxShadow = '0 1px 5px #999999';
-        to_top.style.display = 'flex';
-    } else {
-        navigation_panel.style.boxShadow = 'none';
-        to_top.style.display = 'none';
-    }
-}*/
-
+//OPEN NEW WINDOW AND REDIRECT FACEBOOK PAGE
 fb_link.addEventListener('click', function(e){
     e.preventDefault();
     window.open('https://web.facebook.com/mrTralala89/');
 });
 
-//alert(window.innerWidth);
+//CHANGE NAVIGATION BAR BACKGROUND COLOR
+window.onscroll = function(){
+    if (window.pageYOffset > window.innerHeight-400){
+        to_top.style.display = 'flex';
+    } else {
+        to_top.style.display = 'none';
+    }
+}
+
+
