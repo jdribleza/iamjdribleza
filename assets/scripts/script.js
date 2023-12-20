541bc8c60ba7ebc2aa6e85bdbde026bb5b875c95
Jann Dervin Ribleza <136655670+jdribleza@users.noreply.github.com>
2023-12-20 08:51:39 +0800

    new update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,46 +1,7 @@
-let element = (id) =>{
-    let el = document.getElementById(id);
-    return el;
-}
+let download_btn = document.getElementById("download_cv");
 
-let c_element = (classN) =>{
-    let c_el = document.querySelector(classN);
-    return c_el;
-}
-
-window.onload = function(){
-    element('iamjdribleza').style.transform = 'none';
-    element('prologue-msg').style.transform = 'none';
-}
-
-/*
-(function(){
-    loadPrologue();
-})();*/
-
-
-/*
-document.onscroll = function(){
-    if (window.innerHeight - 300 < window.scrollY) {
-        element('navigation-bar').style.background = '#FFFFFF';
-    } else {
-        element('navigation-bar').style.background = 'transparent';
-    }
-}*/
-
-window.onscroll = function(){
-    var scroll = window.scrollY;
-    if (scroll > 500) {
-        element('brdr-fx').style.visibility = 'visible';
-        element('brdr-fx').style.height = '100%';
-        element('brdr-fx').style.width = '100%';
-        element('about-title').style.color = 'red';
-    } else {
-        element('brdr-fx').style.visibility = 'hidden';
-        element('brdr-fx').style.height = '0';
-        element('brdr-fx').style.width = '0';
-        element('about-title').style.color = 'red';
-    }
-}
-
-
+// execute the download resume button
+download_btn.onclick = function (e) {
+    e.preventDefault();
+    location.href = "assets/docs/iamjdribleza.docx";
+};
