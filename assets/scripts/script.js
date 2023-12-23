c9c28197022cf9d54c4176db2ac512c755249d34
Jann Dervin Ribleza <136655670+jdribleza@users.noreply.github.com>
2023-12-23 10:51:28 +0800

    Latest portfolio update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,33 @@
-let download_btn = document.getElementById("download_cv");
 
-// execute the download resume button
-download_btn.onclick = function (e) {
+const links = document.getElementsByClassName("social_link");
+
+const linkPressed = e => {
+    // prevent the element from navigating when clicked
     e.preventDefault();
-    location.href = "assets/docs/iamjdribleza.docx";
-};
+
+    // get the element id
+    let element_id = e.target.getAttribute("id");
+
+    let link;
+
+     switch (element_id){
+        case "fb_link" : link = "https://web.facebook.com/iamjdribleza/";break;
+        case "gh_link" : link = "https://github.com/jdribleza";break;
+        default : link = "https://www.linkedin.com/in/jdribleza-9b252427b";break;
+    }
+
+    // open new window and redirect to specified link
+    window.open(link);
+
+}
+
+// loop links to add event when clicked
+for(let link of links){
+    link.addEventListener("click", linkPressed);
+}
+
+// For download CV button
+document.getElementById("download_cv").onclick = e => {
+    e.preventDefault();
+    window.location = "assets/docs/iamjdribleza.docx";
+}
